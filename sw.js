659aa37fe8a3bb3d5a1031d2ec6aa1b8b8fd2011
vicgom892 <43424636+vicgom892@users.noreply.github.com>
2026-03-09 19:32:13 -0300

    Listo

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'gtc-panel-v1';
 const urlsToCache = [
-  '/',
-  'index.html',
-  'manifest.json',
-  'icon-192x192.png',
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192x192.png',
   'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
   'https://cdn.jsdelivr.net/npm/chart.js'
