524b0c375fe35367c024320c7139ed0656cc1c9b
vicgom892 <vicgom892@gmail.com>
2026-03-10 12:20:59 -0300

    sw.js cambio

--- sw.js
+++ sw.js
@@ -12,7 +12,10 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icon-192x192.png'
+  '/icon-192x192.png',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
+  'https://cdn.jsdelivr.net/npm/chart.js'
 ];
 
 // Recursos que se actualizan frecuentemente (estrategia network-first)
